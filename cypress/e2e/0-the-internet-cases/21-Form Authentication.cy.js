5b9bf761a5d92bd88be3c419254c8c8ae5143017
FedenaK <federiconakazato@gmail.com>
2024-07-26 10:57:49 -0300

    ADD - More cases to FormAuthentication Test

--- cypress/e2e/0-the-internet-cases/21-Form Authentication.cy.js
+++ cypress/e2e/0-the-internet-cases/21-Form Authentication.cy.js
@@ -6,7 +6,7 @@ describe('Form Authentication', () => {
         cy.visit('https://the-internet.herokuapp.com/')
     })
 
-    it('Se van a seleccionar objetos de una lista', () => {
+    it('001 - Se va a ingresar el usuario y contraseña correcta', () => {
         const visitObj = new VisitPage();
         
         visitObj.HomeTexts();
@@ -36,7 +36,54 @@ describe('Form Authentication', () => {
         //Logout
         cy.get('.button').should('contain','Logout').and('have.css','background-color','rgb(233, 233, 233)').click()
         cy.intercept('GET', 'https://298279967.log.optimizely.com/event*', { statusCode: 200, body: {} });
-        cy.url('/login')
+        cy.url('/login');
+    });
 
+    it('002 - Se va a ingresar el usuario incorrecto y contraseña correcta, luego validar el mensaje de error del usuario', () => {
+        const visitObj = new VisitPage();
+        
+        visitObj.HomeTexts();
+        visitObj.FormAuthentication();
+
+        cy.intercept('GET', 'https://298279967.log.optimizely.com/event*', { statusCode: 200, body: {} });
+        cy.url('/login')
+        cy.get('h2').should('be.visible').and('have.text','Login Page')
+        //Select by name:
+        cy.get('h4.subheader').should('contain','log into the secure area');
+        //User
+        cy.get(':nth-child(1) > .large-6').contains('Username')
+        cy.get('#username').type('tommy');
+        //Password
+        cy.get(':nth-child(2) > .large-6').contains('Password')
+        cy.get('#password').type('SuperSecretPassword!');
+        //Login Button
+        cy.get('button.radius').contains('Login').click();
+
+        cy.get('#flash').should('be.visible').and('have.css','background-color','rgb(198, 15, 19)').should('contain','Your username is invalid!')
+    });
+
+    it('003 - Se va a ingresar el usuario correcto, pero con la contraseña incorrecta y validar el mensaje de error de la contraseña', () => {
+        const visitObj = new VisitPage();
+        
+        visitObj.HomeTexts();
+        visitObj.FormAuthentication();
+
+        cy.intercept('GET', 'https://298279967.log.optimizely.com/event*', { statusCode: 200, body: {} });
+        cy.url('/login')
+        cy.get('h2').should('be.visible').and('have.text','Login Page')
+        //Select by name:
+        cy.get('h4.subheader').should('contain','log into the secure area');
+        //User
+        cy.get(':nth-child(1) > .large-6').contains('Username')
+        cy.get('#username').type('tomsmith');
+        //Password
+        cy.get(':nth-child(2) > .large-6').contains('Password')
+        cy.get('#password').type('SuperIncorrectPassword!');
+        //Login Button
+        cy.get('button.radius').contains('Login').click();
+
+        cy.get('#flash').should('be.visible').and('have.css','background-color','rgb(198, 15, 19)').should('contain','Your password is invalid!')
     })
+
+
 })
