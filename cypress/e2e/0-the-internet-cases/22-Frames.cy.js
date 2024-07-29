318394d196f12d5654c682c135ae7c8d73d602bd
FedenaK <federiconakazato@gmail.com>
2024-07-29 17:30:13 -0300

    ADD/CHANGES - Update iFrame folder to Frame. Add bottom Frame

--- cypress/e2e/0-the-internet-cases/22-Frames.cy.js
+++ cypress/e2e/0-the-internet-cases/22-Frames.cy.js
@@ -1,5 +1,5 @@
 import VisitPage from "../../pageObjects/visitPage";
-import IframePage from "../../pageObjects/iFrames/iframesPage.cy";
+import FramePage from "../../pageObjects/framesPage/framesPage.cy";
 
 describe('Frames', () => {
     beforeEach('', () => {
@@ -9,7 +9,7 @@ describe('Frames', () => {
 
     it('Se van a probar los "Nested Frames"', () => {
         const visitObj = new VisitPage();
-        const iFrameObj = new IframePage();
+        const FrameObj = new FramePage();
         
         visitObj.HomeTexts();
         visitObj.Frames();
@@ -17,18 +17,11 @@ describe('Frames', () => {
         cy.intercept('GET', 'https://298279967.log.optimizely.com/event*', { statusCode: 200, body: {} });
         cy.url('/frames')
         cy.get(':nth-child(1) > a').should('have.text','Nested Frames').click()
-        //cy.get('frameset').get('frame').eq(0).should('have.attr','src','/frame_top')
-        //.its('0.document')
-        //cy.get('frame[src="/frame_top"]').its('0.contentDocument').should('exist')
-        /*getIframeTopBody()
-        .find('frame:nth-child(1)').should('have.attr','src','/frame_left');
-        getIframeTopBody()
-        .find('frame:nth-child(2)').should('have.attr','src','/frame_middle');
-        getIframeTopBody()
-        .find('frame:nth-child(3)').should('have.attr','src','/frame_right');*/
-        //getIframeTopValidation()
-        iFrameObj.validateIframes();
 
+        cy.log('Validating Top Frames: ');
+        FrameObj.validateTopFrames();
+        cy.log('Validating Bottom Frames');
+        FrameObj.validateBotFrame();
 
-    })
-})
+    });
+});
